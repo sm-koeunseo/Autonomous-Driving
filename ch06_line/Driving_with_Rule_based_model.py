6845470fa676cd5dc42d9b6dccceb0c8550af559
sm-koeunseo <koeunseo@sookmyung.ac.kr>
2024-06-03 11:40:46 +0900

    [feature] #3 change size & accuracy

--- ch06_line/Driving_with_Rule_based_model.py
+++ ch06_line/Driving_with_Rule_based_model.py
@@ -3,14 +3,16 @@ import numpy as np
 from urllib.request import urlopen
 import threading
 
-# 수정사항 1 : IP를 수정하여 진행하세요.
-ip = '192.168.137.169'
+
+
+# ip 변경 필요
+ip = '192.168.137.202'
 stream = urlopen('http://' + ip + ':81/stream')
 buffer = b''
-# 수정사항 2 : 차량의 속도는 80으로 세팅되어 있으나, 변경하면서 진행하셔도 됩니다.
-urlopen('http://' + ip + "/action?go=speed80")
+urlopen('http://' + ip + "/action?go=speed100") # 차량 속도 변경
 
 image_flag = 0
+
 def image_process_thread():
     global image_flag, car_state
     while True:
@@ -21,15 +23,16 @@ def image_process_thread():
                 urlopen('http://' + ip + "/action?go=right")
             elif car_state == "left":
                 urlopen('http://' + ip + "/action?go=left")
-            
             image_flag = 0
             
-# 데몬 스레드를 생성합니다 (이미지 프로세싱을 별도 스레드에서 진행(원활한 통신을 위해)).
+# 데몬 스레드를 생성 -> 이미지 프로세싱을 별도 스레드에서 진행
 daemon_thread = threading.Thread(target=image_process_thread)
 daemon_thread.daemon = True 
 daemon_thread.start()
 
 car_state = "go"
+left = 30
+right = 10
 while True:
     buffer += stream.read(4096)
     head = buffer.find(b'\xff\xd8')
@@ -41,10 +44,9 @@ while True:
             buffer = buffer[end+2:]
             img = cv2.imdecode(np.frombuffer(jpg, dtype=np.uint8), cv2.IMREAD_UNCHANGED)
             
-            #이미지 전처리 1 : 이미지 크기 조절
+            # 이미지 전처리 1: 이미지 크기 조절 (세로, 가로)
             height, width, _ = img.shape
-# 수정사항 3 : 아래부분 삭제 한 영역(-10)은 변경하셔도 됩니다(카메라 노이즈 제거용)
-            img = img[height // 2:height-10, :]
+            img = img[height // 2:height-20, :-20] # 더 자르고 싶으면 -15
             
             # 색상 필터링으로 검정색 선 추출
             lower_bound = np.array([0, 0, 0])
@@ -62,15 +64,24 @@ while True:
             
             # 무게 중심과 이미지 중앙의 거리 계산
             center_offset = width // 2 - cX
+
             # 디버그용 시각화
             cv2.circle(img, (cX, cY), 10, (0, 255, 0), -1)
+            
+            # 경계값 시각화 (점선으로 표시)
+            left_boundary = width // 2 - left  # 왼쪽 경계
+            right_boundary = width // 2 + right  # 오른쪽 경계
+            cv2.line(img, (width // 2, 0), (width // 2, height), (0, 255, 0), 1, lineType=cv2.LINE_AA, shift=0)
+            cv2.line(img, (left_boundary, 0), (left_boundary, height), (255, 0, 0), 1, lineType=cv2.LINE_AA, shift=0)
+            cv2.line(img, (right_boundary, 0), (right_boundary, height), (255, 0, 0), 1, lineType=cv2.LINE_AA, shift=0)
+            
             cv2.imshow("AI CAR Streaming", img)
 
-# 수정사항 4 : 현재 무게중심 위치가 -10~10은 직진하도록 설계, 이를 변경하여 최적의 성능을 찾아보세요.
-            if center_offset < -10:
+            # 무게중심 기준으로 판단기준 변경
+            if center_offset  < -right:
                 print("오른쪽")
                 car_state = "right"
-            elif center_offset > 10:
+            elif center_offset  > left:
                 print("왼쪽")
                 car_state = "left"
             else:
